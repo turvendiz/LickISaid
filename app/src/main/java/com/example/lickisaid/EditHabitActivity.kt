2016718f25940f89e97405e80cc8b9fca7b2d02f
tundi <tundi@daotech.ru>
2019-06-10 00:32:27 +0400

    created activity_edit_habit added icons

---
+++ app/src/main/java/com/example/lickisaid/EditHabitActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lickisaid
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class EditHabitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_habit)
+    }
+}
